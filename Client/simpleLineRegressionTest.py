dd8f29f52f2316876d59843b11d190a1124a45dd
ricruan <ric@ruan.com>
2025-10-16 17:03:51 +0800

    u

--- Client/simpleLineRegressionTest.py
+++ Client/simpleLineRegressionTest.py
@@ -12,7 +12,11 @@ random_ds = DataSetModel.get_random_liner_regression_ds(seed=47)
 
 # 数据集数据
 df = pd.read_csv('../DataSet/Student_Performance.csv')
+# 将 "Yes"/"No" 转为 1/0
+df['Extracurricular Activities'] = df['Extracurricular Activities'].map({'Yes': 1, 'No': 0})
+
 student_ds = DataSetModel(X= df[['Hours Studied']].values,Y= df[['Performance Index']].values,type='liner')
+student_ds_multi = DataSetModel(X= df.drop('Performance Index', axis=1).values,Y= df[['Performance Index']].values,type='liner')
 print(1)
 
 # 简单一元线性回归类
@@ -20,7 +24,6 @@ class SimpleUnitaryLinearRegression:
     def __init__(self):
         self.slope = None
         self.intercept = None
-        self.sklearn_linear_model = None
 
     def fit(self, X, y):
         """
@@ -36,10 +39,6 @@ class SimpleUnitaryLinearRegression:
 
         # 计算截距 (intercept)
         self.intercept = y_mean - self.slope * x_mean
-
-    def sklearn_fit(self, X, y):
-        self.sklearn_linear_model = LinearRegression()
-        self.sklearn_linear_model.fit(X, y)
 
     def predict(self, X, y = None):
         if self.slope is None or self.intercept is None:
@@ -57,30 +56,37 @@ if __name__ == '__main__':
 
     # 待预测值
     predict_value = 14
-    # 创建并训练模型
+    student_test_data = [[12,56,0,9,12]] # 学习时间、以前的分数、课外活动、睡眠时间、练习的样卷、成绩指数
+
+    # 手撕 一元线性回归模型
     model = SimpleUnitaryLinearRegression()
-
-    skl_model = SimpleUnitaryLinearRegression()
-
-    # 手撕
     model.fit(x.flatten(),y.flatten())
     y_pred = model.predict(x.flatten())
-    # 框架
-    skl_model.sklearn_fit(x, y)
-    y_pred_skl = skl_model.sklearn_linear_model.predict(x)
+    # 框架 一元线性回归模型
+    skl_model = LinearRegression()
+    skl_model.fit(x, y)
+    y_pred_skl = skl_model.predict(x)
+    # 框架 多元线性回归模型
+    skl_model_mul = LinearRegression()
+    skl_model_mul.fit(student_ds_multi.X,student_ds_multi.Y)
+    y_pred_skl_mul = skl_model_mul.predict(student_test_data)
 
     # 输出结果
     print(f"手撕 拟合的斜率（slope）: {model.slope:.2f}")
     print(f"手撕 拟合的截距（intercept）: {model.intercept:.2f}")
     print(f"手撕 预测 {predict_value} 的值: {model.predict(predict_value):.2f}")
 
-    print(f"框架 拟合的斜率（slope）: {skl_model.sklearn_linear_model.coef_[0]}")
-    print(f"框架 拟合的截距（intercept）: {skl_model.sklearn_linear_model.intercept_}")
-    print(f"框架 预测 {predict_value} 的值: {skl_model.sklearn_linear_model.predict([[predict_value]])}")
+    print(f"框架 拟合的斜率（slope）: {skl_model.coef_[0]}")
+    print(f"框架 拟合的截距（intercept）: {skl_model.intercept_}")
+    print(f"框架 预测 {predict_value} 的值: {skl_model.predict([[predict_value]])}")
+
+    print(f"多元预测值 {student_test_data} 的值： {y_pred_skl_mul} ")
+
     # 可视化
     plt.scatter(x, y, color='blue', label='真实数据')
     plt.plot(x, y_pred, color='red', label='手撕拟合直线')
     plt.plot(x, y_pred_skl, color='green', label='框架拟合直线')
+    # plt.plot(x, y_pred_skl_mul, color='yellow', label='框架多元拟合直线')
     plt.xlabel('X')
     plt.ylabel('y')
     plt.legend()
