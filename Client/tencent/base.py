1d95ebca01234411e932b8b4fa634ed6215b3bbc
ricruan <ric@ruan.com>
2025-12-22 16:57:42 +0800

    tencent ci

---
+++ Client/tencent/base.py
@@ -0,0 +1,23 @@
+import os
+
+from dotenv import load_dotenv
+from qcloud_cos import CosConfig
+from qcloud_cos import CosS3Client
+
+import sys
+import logging
+
+load_dotenv()
+# 正常情况日志级别使用 INFO，需要定位时可以修改为 DEBUG，此时 SDK 会打印和服务端的通信信息
+logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+
+# 1. 设置用户属性, 包括 secret_id, secret_key, region 等。Appid 已在 CosConfig 中移除，请在参数 Bucket 中带上 Appid。Bucket 由 BucketName-Appid 组成
+secret_id = os.getenv("TC_SECRET_ID")     # 替换为用户的 SecretId，请登录访问管理控制台进行查看和管理，https://console.cloud.tencent.com/cam/capi
+secret_key = os.getenv("TC_SECRET_KEY")   # 替换为用户的 SecretKey，请登录访问管理控制台进行查看和管理，https://console.cloud.tencent.com/cam/capi
+region = os.getenv("TC_REGION")     # 替换为用户的 region，已创建桶归属的 region 可以在控制台查看，https://console.cloud.tencent.com/cos5/bucket
+                           # COS支持的所有 region 列表参见 https://cloud.tencent.com/document/product/436/6224
+token = None               # 如果使用永久密钥不需要填入token，如果使用临时密钥需要填入，临时密钥生成和使用指引参见 https://cloud.tencent.com/document/product/436/14048
+scheme = os.getenv("TC_SCHEME")           # 指定使用 http/https 协议来访问 COS，默认为 https，可不填
+
+config = CosConfig(Region=region, SecretId=secret_id, SecretKey=secret_key, Token=token, Scheme=scheme)
+client = CosS3Client(config)
---
+++ Client/tencent/ci_auditing_text.py
@@ -0,0 +1,33 @@
+import os
+
+from dotenv import load_dotenv
+
+from Client.tencent.base import client
+
+load_dotenv()
+
+def ci_auditing_text_submit():
+    # 用户自定义业务字段
+    user_info = {
+        'TokenId': '123456',  # 一般用于表示账号信息，长度不超过128字节
+        'Nickname': '测试',  # 一般用于表示昵称信息，长度不超过128字节
+        'DeviceId': '腾讯云',  # 一般用于表示设备信息，长度不超过128字节
+        'AppId': '12500000',  # 一般用于表示 App 的唯一标识，长度不超过128字节
+        'Room': '1',  # 一般用于表示房间号信息，长度不超过128字节
+        'IP': '127.0.0.1',  # 一般用于表示 IP 地址信息，长度不超过128字节
+        'Type': '测试',  # 一般用于表示业务类型，长度不超过128字节
+        'ReceiveTokenId': '789123',  # 一般用于表示接收消息的用户账号，长度不超过128字节
+        'Gender': '男',  # 一般用于表示性别信息，长度不超过128字节
+        'Level': '100',  # 一般用于表示等级信息，长度不超过128字节
+        'Role': '测试人员',  # 一般用于表示角色信息，长度不超过128字节
+    }
+    response = client.ci_auditing_text_submit(
+        Bucket=os.getenv("TC_BUCKET_NAME"),  # 桶名称
+        Content='123456test'.encode("utf-8"),  # 需要审核的文本内容
+        BizType='',  # 表示审核策略的唯一标识
+        UserInfo=user_info,  # 用户自定义业务字段
+        DataId='456456456',  # 待审核的数据进行唯一业务标识
+    )
+    print(response)
+
+ci_auditing_text_submit()
