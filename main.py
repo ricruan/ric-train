3340749874b01a4378649491ca3ad727344aefaf
Ricruan <8897449+ricruan@user.noreply.gitee.com>
2026-07-16 17:03:31 +0800

    test

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 
 
-@app.post("/execute_sql")
+@app.post("/execute_sql1")
 def read_root(sql:str= Body(..., embed=True)):
     result = MySQLClient().execute_sync(sql)
     return result
